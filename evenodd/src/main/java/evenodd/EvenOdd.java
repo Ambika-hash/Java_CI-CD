2be2c6a70c2ca3c389d77337f5d36bd943221a38
Ambika-hash <ambika.m.sharma2005@gmail.com>
2026-02-04 11:21:07 +0530

    Remove input prompt comment and fix typo in output
    Removed commented prompt for user input and fixed typo in output message.

--- evenodd/src/main/java/evenodd/EvenOdd.java
+++ evenodd/src/main/java/evenodd/EvenOdd.java
@@ -4,7 +4,6 @@ public class EvenOdd {
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		//System.out.print("Enter: ");
 		int input = s.nextInt();
 		if (input%2==0) {
 			System.out.print(n+ " is even numbeer");
