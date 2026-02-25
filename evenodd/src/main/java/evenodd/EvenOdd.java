08f8cbf1856f8456164fb4bc425973f00018bc6a
Ambika-hash <ambika.m.sharma2005@gmail.com>
2026-02-25 11:12:28 +0530

    Fix formatting of if condition in EvenOdd.java

--- evenodd/src/main/java/evenodd/EvenOdd.java
+++ evenodd/src/main/java/evenodd/EvenOdd.java
@@ -6,7 +6,7 @@ public class EvenOdd {
 		System.out.print("Enter number: ");
 		Scanner s = new Scanner(System.in);
 		int input = s.nextInt();
-		if (input%2==0) {
+		if (input % 2==0) {
 			System.out.print(n+ " is even numbeer");
 		}else {
 			System.out.print(n+" is Odd number");
